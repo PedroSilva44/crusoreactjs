98e4bd2380e5c27a1fed5850b3af1e44839b1431
PedroSilva44 <pedrohss.496@gmail.com>
2025-10-21 00:11:01 -0300

    consertei o alt

--- src/pages/contato/index.jsx
+++ src/pages/contato/index.jsx
@@ -12,7 +12,7 @@ export default function Contato() {
             </Link> 
 
             <h1 className='titulo1'>Contato</h1>
-            <img className='icone' src="/assets/images/contato.png" />
+            <img className='icone' src="/assets/images/contato.png" alt="ícone de contato"/>
         </div>
     )
 }
